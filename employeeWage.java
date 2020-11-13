208f7bbe1d0d6820b88ac349aaf7b307f5e751cc
“Enter <rahulrajan5410@gmail.com>
2020-11-13 16:21:45 +0530

    added full time employes

--- employeeWage.java
+++ employeeWage.java
@@ -17,3 +17,15 @@ else
 }
 >>>>>>> UC1-Employee_Attendence
 }
+public class employeeWage {
+	
+public static final int IS_PART_TIME = 1;
+public static final int IS_FULL_TIME = 2;
+public static final int EMP_RATE_PER_HOUR = 20;
+public static final int NUM_OF_WORKING_DAYS = 20;
+public static final int MAX_HOURS_IN_MONTH = 160;
+
+
+	public static void main(String[] args) {
+
+int totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays = 0;
