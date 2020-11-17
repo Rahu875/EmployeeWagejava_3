664f335ea3c393624651c04a8518d81b3ca006b3
“Enter <rahulrajan5410@gmail.com>
2020-11-17 18:05:16 +0530

    added arraylist

--- interface.java
+++ interface.java
@@ -30,3 +30,24 @@ public void setTotalEmpWage(int totalEmpWage) {
 public String toString() {
 	return "Total Emp Wage For Company:" +company+"is:"+totalEmpWage;
 	}
+}
+public class EmpWageBuilder implements ComputeEmpWage {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	
+	private int numOfCompany = 0;
+	private LinkedList<CompanyEmpWage> companyEmpWageList;
+	private Map<String,CompanyEmpWage> companyToEmpWageMap;
+	public EmpWageBuilder() {
+		companyEmpWageList = new LinkedList<CompanyEmpWage>();
+		companyToEmpWageMap = new HashMap<>();
+		
+	}
+
+	public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth) {
+		CompanyEmpWage companyEmpWage = new CompanyEmpWage(company,empRatePerHour,numOfWorkingDays,maxHoursPerMonth);
+	
+	companyEmpWageList.add(companyEmpWage);
+	companyToEmpWageMap.put(company, companyEmpWage);
+}
+	
